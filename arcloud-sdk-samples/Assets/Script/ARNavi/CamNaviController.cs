ab89f0ac27bf88ae56509f18b6719e619875c8df
ogasawarasatoru <world.wide.web.13@gmail.com>
2021-04-20 23:57:20 +0900

    ARNaviカメラ実装

--- arcloud-sdk-samples/Assets/Script/ARNavi/CamNaviController.cs
+++ arcloud-sdk-samples/Assets/Script/ARNavi/CamNaviController.cs
@@ -10,8 +10,9 @@ public class CamNaviController : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("カメラの処理実行確認");
-        StartCoroutine(Authrization()); 
+        Debug.Log("(0)カメラの処理実行確認");
+
+        StartCoroutine(Authrization());
         // StartCamera();
         // var webCamTexture = new WebCamTexture();
         // RawImage.texture = webCamTexture;
@@ -20,12 +21,12 @@ public class CamNaviController : MonoBehaviour
 
     IEnumerator Authrization()
     {
-        Debug.Log("IEnumerator Authrization: カメラの使用許可を確認");
+        Debug.Log("(0)IEnumerator Authrization: カメラの使用許可を確認");
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
         // ユーザーがカメラ起動の許可をしていた場合はカメラを起動
         if (Application.HasUserAuthorization(UserAuthorization.WebCam))
         {
-            Debug.Log("ユーザー許可：カメラを起動します");
+            Debug.Log("(0)ユーザー許可：カメラを起動します");
             // UnityのWebCamTextureで処理
             StartCoroutine(_startCamera());
         }
@@ -41,37 +42,57 @@ public class CamNaviController : MonoBehaviour
 
     IEnumerator _startCamera()
     {
+        var width = RawImage.GetComponent<RectTransform>().sizeDelta.x;
+        var height = RawImage.GetComponent<RectTransform>().sizeDelta.y;
+        int Pwidth = (int)width;
+        int Pheight = (int)height;
+        // int Pwidth = 828;
+        // int Pheight = 1792;
+        Debug.Log("width: " + Pwidth + ",height: " + Pheight);
         Debug.Log("_startCamera(): 実行");
         // 指定カメラを起動させる
-        webCam = new WebCamTexture(WebCamTexture.devices[0].name, 640, 480);
+        webCam = new WebCamTexture(WebCamTexture.devices[0].name, Pwidth, Pheight);
         Debug.Log("(1)カメラのテクスチャ取得→(2)RawImageのオイラー角を取得");
         Vector3 angles = RawImage.GetComponent<RectTransform>().eulerAngles;
         Debug.Log("(1)(2)→(3)カメラテクスチャの右回りの角度を-x°の形式で取得してオイラー角のZ軸に代入");
         // カメラ起動
         webCam.Play();
-        Debug.Log("webCam.Play()起動");
+        Debug.Log("(1)(2)(3)→(4)webCam.Play()起動");
         angles.z = -webCam.videoRotationAngle;
-        Debug.Log("(1)(2)(3)→(4)RawImageのRectTransformにオイラー角を代入");
+        Debug.Log("(1)(2)(3)(4)→(5)RawImageのRectTransformにオイラー角を代入");
         RawImage.GetComponent<RectTransform>().eulerAngles = angles;
         // widthはx、heightはyでサイズ調整
         // 全体を表示させるように、大きい方のサイズを表示枠に合わせる
         float scaler;
-        Vector2 sizemp = RawImage.GetComponent<RectTransform>().sizeDelta;
-        Debug.Log("(1)(2)(3)(4)→(5)"+ sizemp);
-        // if()
-
-
+        Vector2 sizetmp = RawImage.GetComponent<RectTransform>().sizeDelta;
+        Debug.Log("(1)(2)(3)(4)(5)→(6)"+ sizetmp);
+        // if()        
         // RawImageのテクスチャにWebCamTextureのインスタンスを設定
-        RawImage.texture = webCam;       
-        Debug.Log("RawImageのテクスチャにwebCamを代入");
-      
-
-        while(webCam.width != webCam.requestedWidth)
+        if(webCam.width > webCam.height)
         {
-            Debug.Log("webCam.widthの幅がrequestした高さと合ってなければ次のフレームで再開");
-            // widthが指定したものになっていない場合は処理を抜けて次のフレームで再開
-            yield return null;
+            scaler = sizetmp.x / webCam.width;
+            Debug.Log("(1)(2)(3)(4)(5)(6)→(7)幅 > 高さ");
         }
+        else
+        {
+            scaler = sizetmp.y / webCam.height;
+            Debug.Log("(1)(2)(3)(4)(5)(6)→(7)幅 < 高さ");
+        }
+        Debug.Log("(1)(2)(3)(4)(5)(6)(7)→(8)幅高さ調整");
+        // サイズ調整
+        sizetmp.x = webCam.width * scaler;        
+        sizetmp.y = webCam.height * scaler;        
+        RawImage.GetComponent<RectTransform>().sizeDelta = sizetmp;
+        Debug.Log("(1)(2)(3)(4)(5)(6)(7)(8)→(9)RawImageのテクスチャにwebCamを代入");
+
+        RawImage.texture = webCam;       
+        Debug.Log("処理完了");
+        // while(webCam.width != webCam.requestedWidth)
+        // {
+        //     Debug.Log("webCam.widthの幅がrequestした高さと合ってなければ次のフレームで再開");
+        //     // widthが指定したものになっていない場合は処理を抜けて次のフレームで再開
+        //     yield return null;
+        // }
         yield break;
     }
 
